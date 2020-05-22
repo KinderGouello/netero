599f20c2c153c688fbc1fa0c9852e661a6172a3c
Remy Gouello <KinderGouello@users.noreply.github.com>
2020-05-22 22:45:46 +0200

    feat(multi-class): add multi class support (#10)
    Signed-off-by: Rémy Gouello <rigouello@gmail.com>

--- test/mock/withMultipleClasses/index.ts
+++ test/mock/withMultipleClasses/index.ts
@@ -2,6 +2,11 @@ export default {
   services: {
     foo: {
       path: 'mock/withMultipleClasses/foo',
+      class: 'Foo',
+    },
+    bar: {
+      path: 'mock/withMultipleClasses/foo',
+      class: 'Bar',
     },
   },
 };
